4eb3cef339b6c742fd602444c14dfb9d0427cb47
mjc2001 <mjchapman2001@gmail.com>
2022-01-27 12:15:31 +0000

    fs optional project

--- main.go
+++ main.go
@@ -1,18 +1,52 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
 	"ascii-art/Functions"
 )
 
+func CheckBanner(s string) (map[int]string, error) {
+	bannerList := []string{"standard", "shadow", "thinkertoy"}
+
+	for _, i := range bannerList {
+		if i == s {
+			path := "Banners/" + s + ".txt"
+
+			return functions.FileInit(path), nil
+		}
+	}
+
+	return nil, errors.New("Banner not found")
+}
+
 func main() {
 	args := os.Args[1:]
 
+	/* ascii-art
 	if len(args) == 1 {
 		word := args[0]
 		chars := functions.FileInit("Banners/standard.txt")
+
+		result := functions.Transform(word, chars)
+
+		fmt.Print(result)
+	}
+	*/
+
+	message := "Usage: go run . [STRING] [BANNER]\n\nEX: go run . something standard"
+
+	if len(args) == 2 {
+		word := args[0]
+		banner := args[1]
+
+		chars, err := CheckBanner(banner)
+		if err != nil {
+			fmt.Print(message)
+			panic(err)
+		}
 
 		result := functions.Transform(word, chars)
 
