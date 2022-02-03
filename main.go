9ba3059fbf378066ce104dd0c5f608a9e49cc956
Kai-Cheok <kai.cheok@pracedo.com>
2022-02-03 13:08:01 +0000

    testing web page build, commented out ascii art for now

--- main.go
+++ main.go
@@ -4,10 +4,16 @@ import (
 	"errors"
 	"fmt"
 	"os"
-	"strings"
 
 	functions "ascii-art/Functions"
 )
+
+// define webpage structure
+
+type Page struct {
+	Title string
+	Body  []byte
+}
 
 func CheckBanner(s string) (map[int]string, error) {
 	bannerList := []string{"standard", "shadow", "thinkertoy"}
@@ -24,61 +30,88 @@ func CheckBanner(s string) (map[int]string, error) {
 }
 
 func main() {
-	args := os.Args[1:]
+	// args := os.Args[1:]
 
-	/* ascii-art
-	if len(args) == 1 {
-		word := args[0]
-		chars := functions.FileInit("Banners/standard.txt")
+	// /* ascii-art
+	// if len(args) == 1 {
+	// 	word := args[0]
+	// 	chars := functions.FileInit("Banners/standard.txt")
 
-		result := functions.Transform(word, chars)
+	// 	result := functions.Transform(word, chars)
 
-		fmt.Print(result)
+	// 	fmt.Print(result)
+	// }
+	// */
+
+	// /* fs
+	// message := "Usage: go run . [STRING] [BANNER]\n\nEX: go run . something standard"
+
+	// if len(args) == 2 {
+	// 	word := args[0]
+	// 	banner := args[1]
+
+	// 	chars, err := CheckBanner(banner)
+	// 	if err != nil {
+	// 		fmt.Println(message)
+	// 		panic(err)
+	// 	}
+
+	// 	result := functions.Transform(word, chars)
+
+	// 	fmt.Print(result)
+	// }
+	// */
+
+	// message := "Usage: go run . [STRING] [BANNER] [OPTION]\n\nEX: go run . something standard --output=<fileName.txt>"
+
+	// if len(args) == 3 {
+	// 	word := args[0]
+	// 	banner := args[1]
+	// 	output := args[2]
+
+	// 	chars, err := CheckBanner(banner)
+	// 	if err != nil {
+	// 		fmt.Println(message)
+	// 		panic(err)
+	// 	}
+
+	// 	prefix := "--output="
+
+	// 	if strings.HasPrefix(output, prefix) {
+	// 		fileName := strings.TrimPrefix(output, prefix)
+
+	// 		result := functions.Transform(word, chars)
+
+	// 		os.WriteFile(fileName, []byte(result), 0666)
+	// 	} else {
+	// 		fmt.Println(message)
+	// 	}
+	// }
+
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	p1.save()
+	p2, _ := loadPage("TestPage")
+	fmt.Println(string(p2.Body))
+}
+
+/*
+Saves Page's Body to a text file
+This returns an error value beacuse WriteFile has a return type of error. If there's no error then ir returns nil
+0600 indiactes file should be created wiht read-write permissions for current user
+*/
+func (p *Page) save() error {
+	filename := p.Title + ".text"
+	return os.WriteFile(filename, p.Body, 0600)
+}
+
+/*
+Construct file name from title parameter, reads file content into a new variable body and returns pointer to Page literal constructed with proper title and body values
+*/
+func loadPage(title string) (*Page, error) {
+	filename := title + ".text"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
 	}
-	*/
-
-	/* fs
-	message := "Usage: go run . [STRING] [BANNER]\n\nEX: go run . something standard"
-
-	if len(args) == 2 {
-		word := args[0]
-		banner := args[1]
-
-		chars, err := CheckBanner(banner)
-		if err != nil {
-			fmt.Println(message)
-			panic(err)
-		}
-
-		result := functions.Transform(word, chars)
-
-		fmt.Print(result)
-	}
-	*/
-
-	message := "Usage: go run . [STRING] [BANNER] [OPTION]\n\nEX: go run . something standard --output=<fileName.txt>"
-
-	if len(args) == 3 {
-		word := args[0]
-		banner := args[1]
-		output := args[2]
-
-		chars, err := CheckBanner(banner)
-		if err != nil {
-			fmt.Println(message)
-			panic(err)
-		}
-
-		prefix := "--output="
-
-		if strings.HasPrefix(output, prefix) {
-			fileName := strings.TrimPrefix(output, prefix)
-
-			result := functions.Transform(word, chars)
-
-			os.WriteFile(fileName, []byte(result), 0666)
-		} else {
-			fmt.Println(message)
-		}
-	}
+	return &Page{Title: title, Body: body}, nil
 }
